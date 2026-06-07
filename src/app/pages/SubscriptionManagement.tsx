45e06148f787d860ed924fe2955e36c5850d53cc
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-07 21:17:34 +0300

    Update SubscriptionManagement.tsx

--- src/app/pages/SubscriptionManagement.tsx
+++ src/app/pages/SubscriptionManagement.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminNav from '../src/components/AdminNav';
+import AdminNav from '../../components/AdminNav';
 import { Calendar, DollarSign, TrendingUp, AlertCircle, CheckCircle, XCircle } from 'lucide-react';
 
 interface Subscription {
