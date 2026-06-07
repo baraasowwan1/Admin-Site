e508654dabf93554e4771063f931f9b4504cf2e1
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-07 21:13:37 +0300

    Update AdminDashboard.tsx

--- src/app/pages/AdminDashboard.tsx
+++ src/app/pages/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminNav from '../components/AdminNav';
+import AdminNav from '../src/components/AdminNav';
 import { DollarSign, Users, ShoppingBag, TrendingUp, Mail } from 'lucide-react';
 
 export default function AdminDashboard() {
