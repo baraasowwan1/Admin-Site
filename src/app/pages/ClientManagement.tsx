cbe8cd93b7cad51e9db2c5cc8926767cd1a11b21
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-06-07 21:13:21 +0300

    Update ClientManagement.tsx

--- src/app/pages/ClientManagement.tsx
+++ src/app/pages/ClientManagement.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminNav from '../components/AdminNav';
+import AdminNav from '../src/components/AdminNav';
 import { Search, Eye, Mail, Phone, Calendar, DollarSign } from 'lucide-react';
 
 interface Client {
